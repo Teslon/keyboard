8ad132ef645fe8b83811a1fe595f88cc08a7c5b7
Teslon <f.baser@me.com>
2014-04-18 00:58:18 +0200

    Initial Commit

---
+++ keyboard/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  keyboard
+//
+//  Created by MacPro on 18.04.14.
+//  Copyright (c) 2014 FabioBaser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
